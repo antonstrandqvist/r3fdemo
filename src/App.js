8e02b60adbd00b69b3465d4420abe627306c1fff
Anton Strandqvist <anton.strandqvist@umain.com>
2024-05-23 14:29:49 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Sphere, OrbitControls, Environment, Text3D, MeshWobbleMaterial } from '@react-three/drei';
+import {Model} from './Model'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <Sphere position={[-2,0,0]} args={[1, 20]}>
+          <MeshWobbleMaterial factor={2} speed={1} >
+          <meshPhysicalMaterial color={'red'} metalness={1} reflectivity={1} envMapIntensity={1} />
+          </MeshWobbleMaterial>
+        </Sphere>
+       <OrbitControls />
+       <Text3D size={0.5} font="/inter.json"> 
+        Hello world
+        <meshStandardMaterial color="white" />
+       </Text3D>
+       <Model />
+       <Environment preset='sunset' background />
+      </Canvas>
     </div>
   );
 }
